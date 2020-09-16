1df22bd770bde1ceecd3785d1b8bc36408de41a3
danytorresbmx <danytorresbmx@gmail.com>
2020-09-16 16:19:13 -0500

    Vercion completa

--- make.py
+++ make.py
@@ -1,19 +1,21 @@
 from textwrap import wrap
 
+# Recibe el arreglo de letras y empieza a generar el grind de 8x8
 def makeGrind(letters):
     grind = []
-    letters = letters.lower()
+    letters = letters.lower() # Pasa a minusculas las letras para poder compararlas y todas esten en minusculas
     makeFilas = wrap(letters, 8)
 
     for columnas in makeFilas:
         makeColumnas = wrap(columnas, 1)
-        makeColumnas.append("")
+        makeColumnas.append("") # Agrega una columna extra vacia, esto para evitar el error de numeros negativos en el barrido de palabras
         grind.append(makeColumnas)
     
-    grind.append(["","","","","","","","",""])
+    grind.append(["","","","","","","","",""]) # Agrega una fila extra vacia para evitar el error de numeros negativos 
 
     return grind
 
+# Separa cada una de las letras de cada palabra del arreglo de palabras
 def makeWords(words):
     find = []
     for columnas in words:
@@ -22,6 +24,7 @@ def makeWords(words):
 
     return find
 
+# Genera un arreglo con valores False con la misma cantindad de palabras a buscar
 def makeRes(words):
     res = []
     for x in range(len(words)):
